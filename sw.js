fa68441496364973131e5b4d5cb87f21fd76eb82
claudio230289 <claudiofernandogutierrez@gmail.com>
2026-09-16 21:28:46 -0300

    Improve offline caching for Finanzas Lineage PWA

--- sw.js
+++ sw.js
@@ -1,13 +1,29 @@
+const CACHE_NAME = 'finanzas-lineage-v2';
+const APP_SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
+
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+  );
 });
