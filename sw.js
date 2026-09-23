af7be47dca996c109228bc668b358d2dd8250496
claudio230289 <claudiofernandogutierrez@gmail.com>
2026-09-22 22:03:50 -0300

    Add service worker for asset caching
    Implement service worker for caching assets and handling fetch events.

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'finanzas-v13.5-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-finanzas-lineage.svg',
+  './js/db.js',
+  './js/calculos.js',
+  './js/deseos.js',
+  './js/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
