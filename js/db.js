14886e5074cce9f262b9fab3a95aaf6684ab9877
claudio230289 <claudiofernandogutierrez@gmail.com>
2026-09-22 22:37:48 -0300

    Actualizar db.js

--- js/db.js
+++ js/db.js
@@ -5,14 +5,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Configuración de tu proyecto de Firebase
+// Configuración oficial del proyecto LineageApp
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "tu-proyecto.firebaseapp.com",
-    projectId: "tu-proyecto",
-    storageBucket: "tu-proyecto.appspot.com",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
+    apiKey: "AIzaSyA-Seo2AO3STv9YghXE8SiZQ6R8tFS5T2E",
+    authDomain: "lineageapp-7039f.firebaseapp.com",
+    projectId: "lineageapp-7039f",
+    storageBucket: "lineageapp-7039f.firebasestorage.app",
+    messagingSenderId: "526969024739",
+    appId: "1:526969024739:web:e64f5e1884d3c0d13f95d6"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -50,12 +50,11 @@ export function cerrarSesion() {
     });
 }
 
-// Carga de datos vinculada al usuario autenticado
+// Carga de datos vinculada al usuario autenticado (Aislamiento por UID)
 export function cargarBaseDatosRemota() {
     return new Promise((resolve) => {
         onAuthStateChanged(auth, async (user) => {
             if (user) {
-                // Usuario logueado: usamos un documento exclusivo para su UID
                 const docRef = doc(dbFirestore, "finanzas_usuarios", user.uid);
                 try {
                     const snap = await getDoc(docRef);
@@ -69,7 +68,6 @@ export function cargarBaseDatosRemota() {
                 }
                 resolve(db);
             } else {
-                // Si no hay sesión iniciada, forzamos el popup de Google
                 iniciarSesionGoogle();
             }
         });
